b83b3376423bdc8ca541a3e1c753b34a24a01eaf
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-09 17:42:01 +0000

    tailwind button change

---
+++ my-app/src/app/chai/page.tsx
@@ -0,0 +1,16 @@
+function chaiPage() {
+  return (
+      <main className="h-full flex justify-center items-center 
+      flex-col">
+
+      <div>Chai Page 😊
+      </div>
+      <button className="px-6 py-2 bg-blue-500
+      rounded my-3
+      hover:bg-blue-700">test button</button>
+      </main>
+   
+  )
+}
+
+export default chaiPage;
